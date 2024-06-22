51a239271b63325ee1036e181664ce3346b0581e
ELLYANA NOH <ellnoah1411@gmail.com>
2024-06-23 00:12:04 +0800

    added plantListCRUD etc

---
+++ src/main/java/com/heroku/java/controller/PlantController.java
@@ -0,0 +1,201 @@
+package com.heroku.java.controller;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.heroku.java.model.plant;
+
+
+@Controller
+public class PlantController {
+    private final DataSource dataSource;
+
+    @Autowired
+    public PlantController(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+	
+@PostMapping("/addPlant") 
+
+public String addPlant(
+	@RequestParam String plantSciname,
+	@RequestParam String plantComname,
+	@RequestParam String plantType,
+	@RequestParam(required = false) String lightRequirements,
+	@RequestParam(required = false) String humidityPreferences,
+	@RequestParam(required = false) String wateringFrequency,
+	@RequestParam(required = false) String sunlightExposure,
+	@RequestParam(required = false) String windResistance,
+	@RequestParam(required = false) String soilType,
+	@RequestParam String plantHabitat,
+	@RequestParam String plantSpecies,
+	@RequestParam String plantDesc) {
+
+try (Connection connection = dataSource.getConnection()) {
+	String sql = "INSERT INTO public.plants (scientific_name, common_name, type, light_requirements, " +
+                         "humidity_preferences, watering_frequency, sunlight_exposure, wind_resistance, " +
+                         "soil_type, habitat, species, description) " +
+                         "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	final var statement = connection.prepareStatement(sql);
+
+	statement.setString(1, plantSciname);
+	statement.setString(2, plantComname);
+	statement.setString(3, plantType);
+	statement.setString(4, lightRequirements);
+	statement.setString(5, humidityPreferences);
+	statement.setString(6, wateringFrequency);
+	statement.setString(7, sunlightExposure);
+	statement.setString(8, windResistance);
+	statement.setString(9, soilType);
+	statement.setString(10, plantHabitat);
+	statement.setString(11, plantSpecies);
+	statement.setString(12, plantDesc);
+
+	statement.executeUpdate();
+	connection.close();
+}
+ } catch (Exception e) {
+e.printStackTrace();
+//return "redirect:/error";
+} 
+
+return "redirect:/index";
+}
+
+@GetMapping("/plantList")
+public String plantList(Model model) {
+	List<plant> plants = new ArrayList<>();
+
+	try (Connection connection = dataSource.getConnection()) {
+		String sql = "SELECT plantid, scientific_name, common_name, type, habitat, species, description FROM plants ORDER BY plantid";
+		final var statement = connection.prepareStatement(sql);
+		final var resultSet = statement.executeQuery();
+
+		while (resultSet.next()) {
+			int plantId = resultSet.getInt("plantid");
+			String plantSciname = resultSet.getString("scientific_name");
+			String plantComname = resultSet.getString("common_name");
+			String plantType = resultSet.getString("type");
+			String plantHabitat = resultSet.getString("habitat");
+			String plantSpecies = resultSet.getString("species");
+			String plantDesc = resultSet.getString("description");
+
+			plant plant = new plant();
+                plant.setPlantId(plantId);
+                plant.setPlantSciname(plantSciname);
+                plant.setPlantComname(plantComname);
+                plant.setPlantType(plantType);
+                plant.setPlantHabitat(plantHabitat);
+                plant.setPlantSpecies(plantSpecies);
+                plant.setPlantDesc(plantDesc);
+
+			    plants.add(plant);
+		}
+
+		model.addAttribute("plants", plants);
+
+	} catch (SQLException e) {
+		e.printStackTrace();
+		return "error";
+	} 
+
+	return "PlantList";
+}
+
+@GetMapping("/updatePlants")
+public String updatePlant(@RequestParam("plantId") int plantId, Model model) {
+
+    try {
+        Connection connection = dataSource.getConnection();
+		String sql = "SELECT plantid, scientific_name, common_name, type, habitat, species, description FROM plants WHERE plantid = ?";
+        final var statement= connection.prepareStatement(sql);
+		statement.setInt(1, plantId);
+        final var resultSet = statement.executeQuery();
+
+        if (resultSet.next()){
+            
+			String plantSciname = resultSet.getString("scientific_name");
+                String plantComname = resultSet.getString("common_name");
+                String plantType = resultSet.getString("type");
+                String plantHabitat = resultSet.getString("habitat");
+                String plantSpecies = resultSet.getString("species");
+                String plantDesc = resultSet.getString("description");
+
+			    plant plant = new plant();
+                plant.setPlantId(plantId);
+                plant.setPlantSciname(plantSciname);
+                plant.setPlantComname(plantComname);
+                plant.setPlantType(plantType);
+                plant.setPlantHabitat(plantHabitat);
+                plant.setPlantSpecies(plantSpecies);
+                plant.setPlantDesc(plantDesc);
+
+                model.addAttribute("plant", plant);
+
+            connection.close();
+        }
+    }
+    catch(Exception e){
+        e.printStackTrace();
+    }
+    return "Plant/updatePlant";
+}
+
+@PostMapping("/updatePlants")
+public String updatePlant(@ModelAttribute("plant") plant plant) {
+ 
+    try {
+        Connection connection = dataSource.getConnection();
+		String sql = "UPDATE plants SET scientific_name = ?, common_name = ?, type = ?, habitat = ?, species = ?, description = ? WHERE plantid = ?";
+        final var statement=connection.prepareStatement(sql);
+
+        statement.setString(1, plant.getPlantSciname());
+        statement.setString(2, plant.getPlantComname());
+        statement.setString(3, plant.getPlantType());
+        statement.setString(4, plant.getPlantHabitat());
+        statement.setString(5, plant.getPlantSpecies());
+        statement.setString(6, plant.getPlantDesc());
+        statement.setInt(7, plant.getPlantId());
+
+        statement.executeUpdate();
+
+        connection.close();
+        
+
+    } catch (Exception e) {
+        e.printStackTrace();
+        return "redirect:/error";
+    }
+    return "redirect:/plantList";
+}
+
+@PostMapping("/deletePlant")
+public String deletePlant(@RequestParam("plantId") int plantId){
+    try {
+        Connection connection = dataSource.getConnection();
+		String sql = "DELETE FROM plants WHERE plantid = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setLong(1,plantId);
+        statement.executeUpdate();
+
+        connection.close();
+
+    } catch (Exception e) {
+        e.printStackTrace();
+        return "redirect:/errorDelete";
+    }
+    return "redirect:/plantList";
+}
+}
--- src/main/java/com/heroku/java/model/plant.java
+++ src/main/java/com/heroku/java/model/plant.java
@@ -1,5 +1,57 @@
 package com.heroku.java.model;
 
+
 public class plant {
-    
-}
+    private int plantId;
+	private String plantSciname;
+	private String plantComname;
+	private String plantType;
+	private String plantHabitat;
+	private String plantSpecies;
+	private String plantDesc;
+	    	
+	    	public plant() {
+	    	}
+	    	public int getPlantId() {
+	    		return plantId;
+	    	}
+	    	public void setPlantId(int plantId) {
+	    		this.plantId = plantId;
+	    	}
+	    	public String getPlantSciname() {
+	    		return plantSciname;
+	    	}
+	    	public void setPlantSciname(String plantSciname) {
+	    		this.plantSciname = plantSciname;
+	    	}
+	    	public String getPlantComname() {
+	    		return plantComname;
+	    	}
+	    	public void setPlantComname(String plantComname) {
+	    		this.plantComname = plantComname;
+	    	}
+	    	public String getPlantType() {
+	    		return plantType;
+	    	}
+	    	public void setPlantType(String plantType) {
+	    		this.plantType = plantType;
+	    	}
+	    	public String getPlantHabitat() {
+	    		return plantHabitat;
+	    	}
+	    	public void setPlantHabitat(String plantHabitat) {
+	    		this.plantHabitat = plantHabitat;
+	    	}
+	    	public String getPlantSpecies() {
+	    		return plantSpecies;
+	    	}
+	    	public void setPlantSpecies(String plantSpecies) {
+	    		this.plantSpecies = plantSpecies;
+	    	}
+	    	public String getPlantDesc() {
+	    		return plantDesc;
+	    	}
+	    	public void setPlantDesc(String plantDesc) {
+	    		this.plantDesc = plantDesc;
+	    	}
+	    }
