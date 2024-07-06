417c110695e3b045793f8ccd93a9e4e4a6c191d5
ELLYANA NOH <ellnoah1411@gmail.com>
2024-07-07 02:58:16 +0800

    try staff

---
+++ src/main/java/com/heroku/java/model/Staff.java
@@ -0,0 +1,71 @@
+package com.heroku.java.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Staff {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String staffName;
+    private String staffEmail;
+    private String staffPassword;
+
+    // Constructors, getters, and setters
+    public Staff() {
+    }
+
+    public Staff(String staffName, String staffEmail, String staffPassword) {
+        this.staffName = staffName;
+        this.staffEmail = staffEmail;
+        this.staffPassword = staffPassword;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStaffName() {
+        return staffName;
+    }
+
+    public void setStaffName(String staffName) {
+        this.staffName = staffName;
+    }
+
+    public String getStaffEmail() {
+        return staffEmail;
+    }
+
+    public void setStaffEmail(String staffEmail) {
+        this.staffEmail = staffEmail;
+    }
+
+    public String getStaffPassword() {
+        return staffPassword;
+    }
+
+    public void setStaffPassword(String staffPassword) {
+        this.staffPassword = staffPassword;
+    }
+
+    @Override
+    public String toString() {
+        return "Staff{" +
+                "id=" + id +
+                ", staffName='" + staffName + '\'' +
+                ", staffEmail='" + staffEmail + '\'' +
+                ", staffPassword='" + staffPassword + '\'' +
+                '}';
+    }
+}
