2e193a4c168daf59d70af9791c1d574eb979369e
ELLYANANOH14 <168503565+ELLYANANOH14@users.noreply.github.com>
2024-07-08 19:35:01 +0800

    test

--- src/main/java/com/heroku/java/controller/AccountController.java
+++ src/main/java/com/heroku/java/controller/AccountController.java
@@ -1,48 +1,39 @@
 package com.heroku.java.controller;
 
 import com.heroku.java.model.StaffModel;
+import com.heroku.java.repository.StaffRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 
 @Controller
 public class AccountController {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final StaffRepository staffRepository;
 
     @Autowired
-    public AccountController(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public AccountController(StaffRepository staffRepository) {
+        this.staffRepository = staffRepository;
     }
 
     @GetMapping("/registerStaff")
-    public ModelAndView showRegistrationForm() {
-        ModelAndView modelAndView = new ModelAndView("register");
-        modelAndView.addObject("staffModel", new StaffModel());
-        return modelAndView;
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("staffModel", new StaffModel());
+        return "register";
     }
 
-    @PostMapping("/registerStaff")
+    @PostMapping("/register")
     public String registerStaff(@ModelAttribute("staffModel") StaffModel staffModel, HttpSession session) {
-        saveStaffToDatabase(staffModel);
+        staffRepository.saveStaff(staffModel);
 
         // Set loggedInUser session attribute after signup
         session.setAttribute("loggedInUser", staffModel);
 
         return "redirect:/staff/dashboard";
-    }
-
-    private void saveStaffToDatabase(StaffModel staffModel) {
-        String sql = "INSERT INTO staff (staffname, staffemail, staffpassword) VALUES (?, ?, ?)";
-        jdbcTemplate.update(sql,
-                staffModel.getStaffName(),
-                staffModel.getStaffEmail(),
-                staffModel.getStaffPassword());
     }
 }
