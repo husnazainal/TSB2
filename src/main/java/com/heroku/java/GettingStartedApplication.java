ee42d2cbef5423bbf30a0027b7fd263f7781bbf2
ELLYANANOH14 <168503565+ELLYANANOH14@users.noreply.github.com>
2024-07-08 17:44:07 +0800

    Revert "pom"
    This reverts commit ca44329cbe1738c8ecd257dac22a81f0cc7d07c5.

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -11,7 +11,7 @@ import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.Map;
 
-@SpringBootApplication(scanBasePackages = { "com.heroku" })
+@SpringBootApplication
 @Controller
 public class GettingStartedApplication {
     private final DataSource dataSource;
