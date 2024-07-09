15948c69930aa4bacf996cf4a241649af6053deb
ELLYANANOH14 <168503565+ELLYANANOH14@users.noreply.github.com>
2024-07-09 20:09:42 +0800

    start update n delete

--- src/main/java/com/heroku/java/controller/PlantController.java
+++ src/main/java/com/heroku/java/controller/PlantController.java
@@ -15,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -152,47 +153,52 @@ public class PlantController {
         return "plantlist";
     }
 
-    @GetMapping("/updatePlant")
-    public String showUpdatePlantForm(@RequestParam("plantId") int plantId, Model model) {
+    @GetMapping("/updatePlant/{plantId}")
+    public String getUpdatePlantForm(@PathVariable("plantId") int plantId, Model model) {
         try (Connection connection = dataSource.getConnection()) {
-            String sql = "SELECT p.plantid, p.sciname, p.comname, p.type, p.habitat, p.species, p.description, "
-                    + "i.lightr, i.humidp, i.waterf, "
-                    + "o.sune, o.windr, o.soilt "
-                    + "FROM plant p "
-                    + "LEFT JOIN indoor_plant i ON p.plantid = i.plantid "
-                    + "LEFT JOIN outdoor_plant o ON p.plantid = o.plantid "
-                    + "WHERE p.plantid = ?";
-
+            String sql = "SELECT * FROM plant WHERE plantid = ?";
             try (PreparedStatement statement = connection.prepareStatement(sql)) {
                 statement.setInt(1, plantId);
                 try (ResultSet resultSet = statement.executeQuery()) {
                     if (resultSet.next()) {
-                        plant plant;
-                        String type = resultSet.getString("type");
-
-                        if ("Indoor".equals(type)) {
-                            IndoorPlant indoorPlant = new IndoorPlant();
-                            indoorPlant.setLightR(resultSet.getString("lightr"));
-                            indoorPlant.setHumidP(resultSet.getString("humidp"));
-                            indoorPlant.setWaterF(resultSet.getString("waterf"));
-                            plant = indoorPlant;
-                        } else if ("Outdoor".equals(type)) {
-                            OutdoorPlant outdoorPlant = new OutdoorPlant();
-                            outdoorPlant.setSunE(resultSet.getString("sune"));
-                            outdoorPlant.setWindR(resultSet.getString("windr"));
-                            outdoorPlant.setSoilT(resultSet.getString("soilt"));
-                            plant = outdoorPlant;
-                        } else {
-                            plant = new plant();
-                        }
-
-                        plant.setPlantId(plantId);
+                        plant plant = new plant();
+                        plant.setPlantId(resultSet.getInt("plantid"));
                         plant.setSciName(resultSet.getString("sciname"));
                         plant.setComName(resultSet.getString("comname"));
-                        plant.setType(type);
+                        plant.setType(resultSet.getString("type"));
                         plant.setHabitat(resultSet.getString("habitat"));
                         plant.setSpecies(resultSet.getString("species"));
                         plant.setDescription(resultSet.getString("description"));
+
+                        if ("Indoor".equals(plant.getType())) {
+                            sql = "SELECT * FROM indoor_plant WHERE plantid = ?";
+                            try (PreparedStatement indoorStatement = connection.prepareStatement(sql)) {
+                                indoorStatement.setInt(1, plantId);
+                                try (ResultSet indoorResultSet = indoorStatement.executeQuery()) {
+                                    if (indoorResultSet.next()) {
+                                        IndoorPlant indoorPlant = new IndoorPlant();
+                                        indoorPlant.setLightR(indoorResultSet.getString("lightr"));
+                                        indoorPlant.setHumidP(indoorResultSet.getString("humidp"));
+                                        indoorPlant.setWaterF(indoorResultSet.getString("waterf"));
+                                        model.addAttribute("IndoorPlant", indoorPlant);
+                                    }
+                                }
+                            }
+                        } else if ("Outdoor".equals(plant.getType())) {
+                            sql = "SELECT * FROM outdoor_plant WHERE plantid = ?";
+                            try (PreparedStatement outdoorStatement = connection.prepareStatement(sql)) {
+                                outdoorStatement.setInt(1, plantId);
+                                try (ResultSet outdoorResultSet = outdoorStatement.executeQuery()) {
+                                    if (outdoorResultSet.next()) {
+                                        OutdoorPlant outdoorPlant = new OutdoorPlant();
+                                        outdoorPlant.setSunE(outdoorResultSet.getString("sune"));
+                                        outdoorPlant.setWindR(outdoorResultSet.getString("windr"));
+                                        outdoorPlant.setSoilT(outdoorResultSet.getString("soilt"));
+                                        model.addAttribute("OutdoorPlant", outdoorPlant);
+                                    }
+                                }
+                            }
+                        }
 
                         model.addAttribute("plant", plant);
                     }
@@ -200,14 +206,18 @@ public class PlantController {
             }
         } catch (SQLException e) {
             e.printStackTrace();
-            return "error";
+            // Handle exception appropriately, maybe redirect to an error page
+            return "redirect:/error";
         }
-        return "updatePlant";
+
+        return "updatePlant"; // Return the Thymeleaf template name
     }
 
     @PostMapping("/updatePlant")
     @Transactional
-    public String updatePlant(@ModelAttribute("plant") plant plant) {
+    public String updatePlant(@ModelAttribute("plant") plant plant,
+            @ModelAttribute("IndoorPlant") IndoorPlant indoorPlant,
+            @ModelAttribute("OutdoorPlant") OutdoorPlant outdoorPlant) {
         try (Connection connection = dataSource.getConnection()) {
             connection.setAutoCommit(false);
             try {
@@ -223,8 +233,7 @@ public class PlantController {
                     statement.executeUpdate();
                 }
 
-                if (plant instanceof IndoorPlant) {
-                    IndoorPlant indoorPlant = (IndoorPlant) plant;
+                if ("Indoor".equals(plant.getType())) {
                     sql = "UPDATE indoor_plant SET lightr = ?, humidp = ?, waterf = ? WHERE plantid = ?";
                     try (PreparedStatement statement = connection.prepareStatement(sql)) {
                         statement.setString(1, indoorPlant.getLightR());
@@ -233,8 +242,7 @@ public class PlantController {
                         statement.setInt(4, plant.getPlantId());
                         statement.executeUpdate();
                     }
-                } else if (plant instanceof OutdoorPlant) {
-                    OutdoorPlant outdoorPlant = (OutdoorPlant) plant;
+                } else if ("Outdoor".equals(plant.getType())) {
                     sql = "UPDATE outdoor_plant SET sune = ?, windr = ?, soilt = ? WHERE plantid = ?";
                     try (PreparedStatement statement = connection.prepareStatement(sql)) {
                         statement.setString(1, outdoorPlant.getSunE());
@@ -292,4 +300,5 @@ public class PlantController {
         }
         return "redirect:/plantList";
     }
+
 }
