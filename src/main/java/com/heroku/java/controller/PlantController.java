833f47aca017e1bf79abb0d265ef37a39f2705c8
ELLYANANOH14 <168503565+ELLYANANOH14@users.noreply.github.com>
2024-07-09 22:34:14 +0800

    7

--- src/main/java/com/heroku/java/controller/PlantController.java
+++ src/main/java/com/heroku/java/controller/PlantController.java
@@ -276,7 +276,7 @@ public class PlantController {
             e.printStackTrace();
             return "redirect:/error";
         }
-        return "redirect:/plantList";
+        return "redirect:/plantlist";
     }
 
     @PostMapping("/deletePlant")
