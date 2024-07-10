1399154d763aff915e8c897d3980d7a55f6a3770
ELLYANANOH14 <168503565+ELLYANANOH14@users.noreply.github.com>
2024-07-10 22:18:25 +0800

    Revert "try validation test1"
    This reverts commit 390112a9df3ed2ab23f95f02fa6a31b6902fb984.

--- src/main/java/com/heroku/java/model/OutdoorPlant.java
+++ src/main/java/com/heroku/java/model/OutdoorPlant.java
@@ -1,8 +1,8 @@
 package com.heroku.java.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.PrimaryKeyJoinColumn;
-import jakarta.persistence.Table;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "outdoor_plant")
