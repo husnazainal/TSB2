5d01a3aca76d0c5a405e99332633acfac59026c7
ELLYANANOH14 <168503565+ELLYANANOH14@users.noreply.github.com>
2024-07-10 17:56:36 +0800

    test

--- src/main/java/com/heroku/java/controller/FeedbackController.java
+++ src/main/java/com/heroku/java/controller/FeedbackController.java
@@ -16,6 +16,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.heroku.java.model.feedback;
 import com.heroku.java.repository.FeedbackRepository;
 
+
 import jakarta.servlet.http.HttpSession;
 
 @Controller
--- src/main/java/com/heroku/java/repository/FeedbackRepository.java
+++ src/main/java/com/heroku/java/repository/FeedbackRepository.java
@@ -1,8 +1,12 @@
 package com.heroku.java.repository;
 
 import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
 import com.heroku.java.model.feedback;
 
+@Repository
 public interface FeedbackRepository {
     void saveFeedback(feedback feedback);
     List<feedback> getAllFeedback();
